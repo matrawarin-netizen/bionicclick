b6687e43c49d92a013c13fec09cc29889eccb523
mathawarin <songkeart@gmail.com>
2025-11-30 03:49:24 -0500

    Signed-off-by: mathawarin <songkeart@gmail.com>

---
+++ update.sh
@@ -0,0 +1,11 @@
+git init
+git config --global user.email "matrawarin@gmail.com"
+git config --global user.name "matrawarin"
+git add .
+git add --all
+git status
+git commit -am 'matrawarin@gmail.com'
+git remote set-url bionicclick 'https://github.com/matrawarin/bionicclick.git'
+git remote add bionicclick 'https://github.com/matrawarin/bionicclick.git'
+git push -u bionicclick main
+
